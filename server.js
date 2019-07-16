1a5b24feecd173d40e5a5dd21720218a6ac34c97
Syed <syedhussain1@outlook.com>
2019-07-15 23:23:15 -0700

    pre-deploy setup some more

--- server.js
+++ server.js
@@ -1,33 +1,38 @@
-const express = require('express');
-const next = require('next');
-const routes = require('./routes');
+// const express = require('express');
+// const next = require('next');
+// const routes = require('./routes');
 
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = routes.getRequestHandler(app);
+// const dev = process.env.NODE_ENV !== 'production';
+// const app = next({ dev });
+// const handle = routes.getRequestHandler(app);
+// const PORT = process.env.PORT || 3000;
+
+// app
+//   .prepare()
+//   .then(() => {
+//     const server = express();
+
+//     // server.get('/portfolioDetails/:id', (req, res) => {
+//     //   const actualPage = '/portfolioDetails';
+//     //   const queryParams = { id: req.params.id };
+//     //   app.render(req, res, actualPage, queryParams);
+//     // });
+
+//     server.get('*', (req, res) => {
+//       return handle(req, res);
+//     });
+
+//     server.use(handle).listen(PORT, err => {
+//       if (err) throw err;
+//       console.log('> Ready on http://localhost:3000');
+//     });
+//   })
+//   .catch(ex => {
+//     console.error(ex.stack);
+//     process.exit(1);
+//   });
 const PORT = process.env.PORT || 3000;
-
-app
-  .prepare()
-  .then(() => {
-    const server = express();
-
-    // server.get('/portfolioDetails/:id', (req, res) => {
-    //   const actualPage = '/portfolioDetails';
-    //   const queryParams = { id: req.params.id };
-    //   app.render(req, res, actualPage, queryParams);
-    // });
-
-    server.get('*', (req, res) => {
-      return handle(req, res);
-    });
-
-    server.use(handle).listen(PORT, err => {
-      if (err) throw err;
-      console.log('> Ready on http://localhost:3000');
-    });
-  })
-  .catch(ex => {
-    console.error(ex.stack);
-    process.exit(1);
-  });
+server.use(handle).listen(PORT, err => {
+	if (err) throw err;
+	console.log('> Ready on http://localhost:3000');
+});
